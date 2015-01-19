ef90a95d84b1299a5be8b79143379dd63d4b2566
Navv <navv1234@gmail.com>
2015-01-19 12:01:36 +0200

    Yank out json wrapper to sc_js.(c|h)pp.

--- engine/report/sc_highchart.cpp
+++ engine/report/sc_highchart.cpp
@@ -102,11 +102,9 @@ std::string highchart::build_id( const buff_t* buff, const std::string& suffix )
 
 // Init default (shared) json structure
 chart_t::chart_t( const std::string& id_str, const sim_t* sim ) :
-  id_str_( id_str ), height_( 250 ), width_( 550 ), sim_( sim )
+  sc_js_t(), id_str_( id_str ), height_( 250 ), width_( 550 ), sim_( sim )
 {
   assert( ! id_str_.empty() );
-
-  js_.SetObject();
 
   // Select which formatter we use for this chart, depending on sim print_styles
   if ( sim -> print_styles == 1 )
@@ -216,62 +214,6 @@ std::string chart_t::to_xml() const
   str += "]]>";
 
   return str;
-}
-
-rapidjson::Value* chart_t::path_value( const std::string& path_str )
-{
-  std::vector<std::string> path = util::string_split( path_str, "." );
-  rapidjson::Value* v = 0;
-  if ( path.size() < 1 )
-    return v;
-
-  assert( ! util::is_number( path[ 0 ] ) );
-
-  if ( ! js_.HasMember( path[ 0 ].c_str() ) )
-  {
-    rapidjson::Value nv;
-    js_.AddMember( path[ 0 ].c_str(), js_.GetAllocator(), 
-                   nv,                js_.GetAllocator() );
-  }
-
-  v = &( js_[ path[ 0 ].c_str() ] ); 
-
-  for ( size_t i = 1, end = path.size(); i < end; i++ )
-  {
-    // Number is array indexing [0..size-1]
-    if ( util::is_number( path[ i ] ) )
-    {
-      if ( v -> GetType() != rapidjson::kArrayType )
-        v -> SetArray();
-
-      unsigned idx = util::to_unsigned( path[ i ] ), missing = 0;
-      if ( v -> Size() <= idx )
-        missing = ( idx - v -> Size() ) + 1;
-
-      // Pad with null objects, until we have enough
-      for ( unsigned midx = 0; midx < missing; midx++ )
-        v -> PushBack( rapidjson::kNullType, js_.GetAllocator() );
-
-      v = &( (*v)[ rapidjson::SizeType( idx ) ] );
-    }
-    // Object traversal
-    else
-    {
-      if ( v -> GetType() != rapidjson::kObjectType )
-        v -> SetObject();
-
-      if ( ! v -> HasMember( path[ i ].c_str() ) )
-      {
-        rapidjson::Value nv;
-        v -> AddMember( path[ i ].c_str(), js_.GetAllocator(), 
-                        nv,                js_.GetAllocator() );
-      }
-
-      v = &( (*v)[ path[ i ].c_str() ] );
-    }
-  }
-
-  return v;
 }
 
 void chart_t::set_xaxis_max( double max )
@@ -397,96 +339,6 @@ void chart_t::add_simple_series( const std::string& type,
   }
 
   add( "series", obj );
-}
-
-chart_t& chart_t::set( const std::string& path, const char* value_ )
-{
-  if ( rapidjson::Value* obj = path_value( path ) )
-  {
-    rapidjson::Value v( value_, js_.GetAllocator() );
-    *obj = v;
-  }
-  return *this;
-}
-
-chart_t& chart_t::set( const std::string& path, const std::string& value_ )
-{
-  return set( path, value_.c_str() );
-}
-
-chart_t& chart_t::add( const std::string& path, const char* value_ )
-{
-  if ( rapidjson::Value* obj = path_value( path ) )
-  {
-    if ( obj -> GetType() != rapidjson::kArrayType )
-      obj -> SetArray();
-
-    rapidjson::Value v( value_, js_.GetAllocator() );
-    obj -> PushBack( v, js_.GetAllocator() );
-  }
-  return *this;
-}
-
-chart_t& chart_t::add( const std::string& path, const std::string& value_ )
-{
-  return add( path, value_.c_str() );
-}
-
-chart_t& chart_t::add( const std::string& path, rapidjson::Value& value_ )
-{
-  if ( rapidjson::Value* obj = path_value( path ) )
-  {
-    if ( obj -> GetType() != rapidjson::kArrayType )
-      obj -> SetArray();
-
-    obj -> PushBack( value_, js_.GetAllocator() );
-  }
-  return *this;
-}
-
-chart_t& chart_t::add( const std::string& path, double x, double low, double high )
-{
-  if ( rapidjson::Value* obj = path_value( path ) )
-  {
-    if ( obj -> GetType() != rapidjson::kArrayType )
-      obj -> SetArray();
-
-    rapidjson::Value v( rapidjson::kArrayType );
-    v.PushBack( x, js_.GetAllocator() ).PushBack( low, js_.GetAllocator() ).PushBack( high, js_.GetAllocator() );
-
-    obj -> PushBack( v, js_.GetAllocator() );
-  }
-  return *this;
-}
-
-chart_t& chart_t::add( const std::string& path, double x, double y )
-{
-  if ( rapidjson::Value* obj = path_value( path ) )
-  {
-    if ( obj -> GetType() != rapidjson::kArrayType )
-      obj -> SetArray();
-
-    rapidjson::Value v( rapidjson::kArrayType );
-    v.PushBack( x, js_.GetAllocator() ).PushBack( y, js_.GetAllocator() );
-
-    obj -> PushBack( v, js_.GetAllocator() );
-  }
-  return *this;
-}
-
-chart_t& chart_t::set( rapidjson::Value& obj, const std::string& name_, const char* value_ )
-{
-  assert( obj.GetType() == rapidjson::kObjectType );
-
-  rapidjson::Value value_obj( value_, js_.GetAllocator() );
-
-  do_set( obj, name_.c_str(), value_obj );
-  return *this;
-}
-
-chart_t& chart_t::set( rapidjson::Value& obj, const std::string& name, const std::string& value_ )
-{
-  return set( obj, name, value_.c_str() );
 }
 
 time_series_t::time_series_t( const std::string& id_str, const sim_t* sim ) :
--- engine/report/sc_highchart.hpp
+++ engine/report/sc_highchart.hpp
@@ -5,9 +5,7 @@
 #ifndef SC_HIGHCHART_HPP
 #define SC_HIGHCHART_HPP
 
-#include "util/rapidjson/document.h"
-#include "util/rapidjson/stringbuffer.h"
-#include "util/rapidjson/prettywriter.h"
+#include "interfaces/sc_js.hpp"
 
 namespace highchart
 {
@@ -51,12 +49,11 @@ struct data_entry_t
   double value;
 };
 
-struct chart_t
+struct chart_t : public js::sc_js_t
 {
   std::string id_str_;
   std::string toggle_id_str_;
   size_t height_, width_;
-  rapidjson::Document js_;
   const sim_t* sim_;
   std::shared_ptr<chart_formatter_t> formatter;
 
@@ -80,62 +77,6 @@ struct chart_t
   virtual std::string to_target_div() const;
   virtual std::string to_json() const;
   virtual std::string to_xml() const;
-
-  chart_t& set( rapidjson::Value& obj, const std::string& name_, rapidjson::Value& value_ );
-
-  // Set the value of JSON object indicated by path to value_
-  template <typename T>
-  chart_t& set( const std::string& path, const T& value_ );
-  // Set the value of JSON object indicated by path to an array of values_
-  template <typename T>
-  chart_t& set( const std::string& path, const std::vector<T>& values_ );
-  // Set the JSON object property name_ to value_
-  template<typename T>
-  chart_t& set( rapidjson::Value& obj, const std::string& name_, const T& value_ );
-  // Set the JSON object property name_ to a JSON array value_
-  template<typename T>
-  chart_t& set( rapidjson::Value& obj, const std::string& name_, const std::vector<T>& value_ );
-
-  // Specializations for const char* (we need copies), and std::string
-  chart_t& set( const std::string& path, const char* value );
-  chart_t& set( const std::string& path, const std::string& value );
-  chart_t& set( rapidjson::Value& obj, const std::string& name, const char* value_ );
-  chart_t& set( rapidjson::Value& obj, const std::string& name, const std::string& value_ );
-
-  // Add elements to a JSON array indicated by path by appending value_
-  template <typename T>
-  chart_t& add( const std::string& path, const T& value_ );
-  // Add elements to a JSON array indicated by path by appending data
-  template <typename T>
-  chart_t& add( const std::string& path, const std::vector<T>& data );
-
-  // Specializations for adding elements to a JSON array for various types
-  chart_t& add( const std::string& path, rapidjson::Value& obj );
-  chart_t& add( const std::string& path, const std::string& value_ );
-  chart_t& add( const std::string& path, const char* value_ );
-  chart_t& add( const std::string& path, double x, double low, double high );
-  chart_t& add( const std::string& path, double x, double y );
-
-protected:
-  // Find the Value object given by a path, and construct any missing objects along the way
-  rapidjson::Value* path_value( const std::string& path );
-  rapidjson::Value& do_set( rapidjson::Value& obj, const char* name_, rapidjson::Value& value_ )
-  {
-    assert( obj.GetType() == rapidjson::kObjectType );
-
-    return obj.AddMember( name_, js_.GetAllocator(), value_, js_.GetAllocator() );
-  }
-
-  template <typename T>
-  rapidjson::Value& do_insert( rapidjson::Value& obj, const std::vector<T>& values )
-  {
-    assert( obj.GetType() == rapidjson::kArrayType );
-
-    for ( size_t i = 0, end = values.size(); i < end; i++ )
-      obj.PushBack( values[ i ], js_.GetAllocator() );
-
-    return obj;
-  }
 };
 
 struct time_series_t : public chart_t
@@ -166,86 +107,6 @@ struct histogram_chart_t : public chart_t
 {
   histogram_chart_t( const std::string& id_str, const sim_t* sim );
 };
-
-template <typename T>
-chart_t& chart_t::set( const std::string& path, const T& value_ )
-{
-  if ( rapidjson::Value* obj = path_value( path ) )
-    *obj = value_;
-  return *this;
-}
-
-template<typename T>
-chart_t& chart_t::set( const std::string& path, const std::vector<T>& values )
-{
-  if ( rapidjson::Value* obj = path_value( path ) )
-  {
-    if ( obj -> GetType() != rapidjson::kArrayType )
-      obj -> SetArray();
-
-    obj -> Clear();
-
-    do_insert( *obj, values );
-  }
-  return *this;
-}
-
-template <typename T>
-chart_t& chart_t::add( const std::string& path, const T& value_ )
-{
-  if ( rapidjson::Value* obj = path_value( path ) )
-  {
-    if ( obj -> GetType() != rapidjson::kArrayType )
-      obj -> SetArray();
-
-    obj -> PushBack( value_, js_.GetAllocator() );
-  }
-
-  return *this;
-}
-
-template<typename T>
-chart_t& chart_t::add( const std::string& path, const std::vector<T>& values )
-{
-  if ( rapidjson::Value* obj = path_value( path ) )
-  {
-    if ( obj -> GetType() != rapidjson::kArrayType )
-      obj -> SetArray();
-
-    do_insert( *obj, values );
-  }
-  return *this;
-}
-
-template<typename T>
-chart_t& chart_t::set( rapidjson::Value& obj, const std::string& name_, const T& value_ )
-{
-  assert( obj.GetType() == rapidjson::kObjectType );
-
-  rapidjson::Value value_obj( value_ );
-
-  do_set( obj, name_.c_str(), value_obj );
-  return *this;
-}
-
-template<typename T>
-chart_t& chart_t::set( rapidjson::Value& obj, const std::string& name_, const std::vector<T>& value_ )
-{
-  assert( obj.GetType() == rapidjson::kObjectType );
-
-  rapidjson::Value value_obj( rapidjson::kArrayType );
-
-  do_set( obj, name_.c_str(), do_insert( value_obj, value_ ) );
-  return *this;
-}
-
-inline chart_t& chart_t::set( rapidjson::Value& obj, const std::string& name_, rapidjson::Value& value_ )
-{
-  assert( obj.GetType() == rapidjson::kObjectType );
-
-  do_set( obj, name_.c_str(), value_ );
-  return *this;
-}
 }
 
 #endif
